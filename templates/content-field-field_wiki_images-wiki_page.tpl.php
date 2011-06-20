bfac3fedcdbabdcf6da80e02e5fdb5a9b0a7737c
Kasper Garnæs <kasper@reload.dk>
2011-06-20 10:04:17 +0200

    Implemented a range of features:
    - Theming of full wiki nodes with carousel for images
    - Theming of wiki teasers
    - Wiki listing pages

---
+++ templates/content-field-field_wiki_images-wiki_page.tpl.php
@@ -0,0 +1,50 @@
+<?php
+// $Id: content-field.tpl.php,v 1.1.2.5 2008/11/03 12:46:27 yched Exp $
+
+/**
+ * @file content-field.tpl.php
+ * Default theme implementation to display the value of a field.
+ *
+ * Available variables:
+ * - $node: The node object.
+ * - $field: The field array.
+ * - $items: An array of values for each item in the field array.
+ * - $teaser: Whether this is displayed as a teaser.
+ * - $page: Whether this is displayed as a page.
+ * - $field_name: The field name.
+ * - $field_type: The field type.
+ * - $field_name_css: The css-compatible field name.
+ * - $field_type_css: The css-compatible field type.
+ * - $label: The item label.
+ * - $label_display: Position of label display, inline, above, or hidden.
+ * - $field_empty: Whether the field has any valid value.
+ *
+ * Each $item in $items contains:
+ * - 'view' - the themed view for that item
+ *
+ * @see template_preprocess_field()
+ */
+?>
+
+<?php if (!$field_empty) : ?>
+<div class="field <?php print $field_name_css ?>">
+
+  <?php if ($label_display == 'above') : ?>
+    <h3><?php print t($label) ?></h3>
+  <?php endif;?>
+
+  <?php foreach ($items as $delta => &$item) :
+      if ($item['empty']) :
+        unset($items[$delta]);
+      else :
+        $item = $item['view'];
+      endif;
+    endforeach;
+    print (sizeof($items) > 1) ? theme('item_list', $items) : array_pop($items);
+  ?>
+
+  <div class="more">
+    <?php print l(t('See all images in the gallery'), 'node/' . $node->nid . '/wiki-gallery') ?>
+  </div>
+</div>
+<?php endif; ?>
--- templates/node-wiki_page.tpl.php
+++ templates/node-wiki_page.tpl.php
@@ -8,6 +8,36 @@
 ?>
 
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix">
+<?php if ($teaser == TRUE): ?>
+  <div class="picture"><?php print $field_wiki_images[0]['view']; ?></div>
+
+  <div class="content">
+
+    <?php if ($node->title): ?>
+      <h3><?php print l($node->title, 'node/' . $node->nid); ?></h3>
+    <?php endif; ?>
+    
+    <div class="meta">
+      <span class="time">
+        <?php print format_date($node->created, 'custom', "j F Y") ?>
+      </span>
+      <span class="author">
+        <?php print t('by') . ' ' . theme('username', $user); ?>
+      </span>
+
+      <?php print $node->field_library_ref[0]['view'];  ?>
+    </div>
+
+    <?php print $node->field_teaser[0]['value']; ?>
+
+    <?php if (count($taxonomy)) { ?>
+      <div class="taxonomy">
+        <?php print $terms; ?>
+      </div>
+    <?php } ?>    
+    
+  </div>
+<?php else: ?>
 	<div class="content">
 		<?php print $content ?>
 	</div>
@@ -15,5 +45,5 @@
 	<?php if ($links){ ?>
     <?php  print $links; ?>
 	<?php } ?>
-
+<?php endif; ?>
 </div>
